56f5112cd9bbc586885a75450e1499b61e13d4a1
Semanna <semanna.p@gmail.com>
2024-06-03 14:34:25 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -51,6 +51,16 @@ public class Tracker {
         return false;
     }
 
+    public void delete(int id) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return;
+        }
+        System.arraycopy(items, index + 1, items, index, size - index + 1);
+        items[size - 1] = null;
+        size--;
+    }
+
     public Item findById(int id) {
         int index = indexOf(id);
         return index != -1 ? items[index] : null;
