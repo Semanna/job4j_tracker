8aa1460386eef2c539425e7a64a51e1728cdaff4
Semanna <semanna.p@gmail.com>
2024-06-03 15:39:58 +0300

    8. Что такое валидация? [#246864]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -43,17 +43,18 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        if (index != -1) {
+        boolean result = index != -1;
+        if (result) {
             item.setId(id);
             items[index] = item;
-            return true;
         }
-        return false;
+        return result;
     }
 
     public void delete(int id) {
         int index = indexOf(id);
-        if (index == -1) {
+        boolean result = index == -1;
+        if (result) {
             return;
         }
         System.arraycopy(items, index + 1, items, index, size - index + 1);
