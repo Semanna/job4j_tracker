6d44a099345e18c5cd241a9ecc7557bfdcbffe56
Semanna <semanna.p@gmail.com>
2024-08-04 19:27:20 +0300

    12.2. Тесты вывода на консоль [#33585 #505622] - исправление замечаний

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -165,7 +165,7 @@ class StartUITest {
         Tracker tracker = new Tracker();
         Item one = tracker.add(new Item("test4"));
         Input input = new MockInput(
-                new String[] {"0", String.valueOf(one.getName()), "1"}
+                new String[] {"0", one.getName(), "1"}
         );
         UserAction[] actions = new UserAction[]{
                 new FindByNameAction(output),
