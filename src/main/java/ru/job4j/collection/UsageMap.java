99c8d400d6aadca3372f9f4e5257b041640d033e
Semanna <semanna.p@gmail.com>
2024-12-03 21:03:07 +0300

    0. Map, HashMap [#212652 #524307] - исправление замечаний

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("annasemenova@mail.ru", "Semenova Anna Petrovna");
+        map.put("kulikov123@internet.ru", "Kulikov Maxim Leonidovich");
+        map.put("ananas86@mail.ru", "Kovaleva Anastasia Valeryevna");
+        map.put("annasemenova@mail.ru", "Frfrfr Meow Meow");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
